6247664da764716baad51a2f71db838ed753290e
Ivan V <ivann96@gmail.com>
2021-06-10 22:03:17 -0300

    proyecto

---
+++ TestNinja/TestNinja.UnitTests/ReservationTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTest
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            var reservation = new ReservationTest();
+        }
+    }
+}
