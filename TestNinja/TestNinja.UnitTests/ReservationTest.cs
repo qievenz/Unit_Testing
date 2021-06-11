a495a65d2f6aa72dd4071424dffd9c1b8d781009
qievenz <ivann96@gmail.com>
2021-06-10 22:37:04 -0300

    mstest

--- TestNinja/TestNinja.UnitTests/ReservationTest.cs
+++ TestNinja/TestNinja.UnitTests/ReservationTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
@@ -6,9 +7,46 @@ namespace TestNinja.UnitTests
     public class ReservationTest
     {
         [TestMethod]
-        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        public void CanBeCancelledBy_AdminIsCancelling_ReturnsTrue()
         {
-            var reservation = new ReservationTest();
+            var reservation = new Reservation();
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_NotAdminIsCancelling_ReturnsFalse()
+        {
+            var reservation = new Reservation();
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsCancelling_ReturnsTrue()
+        {
+            var userMadeBy = new User { IsAdmin = false };
+            var reservation = new Reservation { MadeBy = userMadeBy };
+
+            var result = reservation.CanBeCancelledBy(userMadeBy);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserIsCancelling_ReturnsFalse()
+        {
+            var user_1 = new User();
+            var user_2 = new User();
+            var reservation = new Reservation { MadeBy = user_1 };
+
+            var result = reservation.CanBeCancelledBy(user_2);
+
+            Assert.IsFalse(result);
         }
     }
 }
