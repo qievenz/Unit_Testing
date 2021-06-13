e89708cea51438f5b2fee2750cfcb6aa6665805c
qievenz <ivann96@gmail.com>
2021-06-13 18:02:53 -0300

    fundamentals

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        [Test]
+        [Ignore("Because is the shiieeet")]
+        public void Add_WhenCalled_ReturnTheSumOfArgs()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArg(int a, int b, int expected)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
