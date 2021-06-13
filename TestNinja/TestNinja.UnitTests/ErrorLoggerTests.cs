ec5b420895841d5cf932da51406883b9ddd21d5f
qievenz <ivann96@gmail.com>
2021-06-13 18:32:28 -0300

    exceptions

---
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SHouldSetLastErrorProperty()
+        {
+            _logger.Log("a");
+
+            Assert.That(_logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgException(string value)
+        {
+            Assert.That(() => _logger.Log(value), Throws.ArgumentNullException);
+        }
+    }
+}
