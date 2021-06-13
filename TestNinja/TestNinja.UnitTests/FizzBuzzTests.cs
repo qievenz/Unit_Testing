a76ce14c1059d0ea1b3c8231989d07c3156d69d2
qievenz <ivann96@gmail.com>
2021-06-13 19:20:32 -0300

    ejercicio

---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_MultiplosDe3_DevuelveFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_MultiplosDe5_DevuelveBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_MultiplosDe3o5o15_DevuelveFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+    }
+}
